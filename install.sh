f7eff6c7fd14345016f1bd96b4bcf1eefc06567e
Wagner Andrade <19WAS85@gmail.com>
2026-05-31 12:25:33 +0000

    feat(install): add omp-update symlink for update.sh

--- install.sh
+++ install.sh
@@ -10,5 +10,6 @@ mkdir ~/.omp
 mkdir -p ~/.local/bin
 ln -sf "$SCRIPT_DIR/run.sh" ~/.local/bin/omp
 ln -sf "$SCRIPT_DIR/build.sh" ~/.local/bin/omp-build
+ln -sf "$SCRIPT_DIR/update.sh" ~/.local/bin/omp-update
 
-echo "[install.sh] Installed: ~/.local/bin/omp, ~/.local/bin/omp-build"
+echo "[install.sh] Installed: ~/.local/bin/omp, ~/.local/bin/omp-build, ~/.local/bin/omp-update"
