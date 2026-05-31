6741066947672263b766ce7f43553ceb1be9a2ff
Wagner Andrade <19WAS85@gmail.com>
2026-05-31 04:07:02 +0000

    feat: add container entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -uo pipefail
+
+omp update
+
+if [[ $# -gt 0 ]] && [[ "$1" != "omp" ]] && command -v "$1" &>/dev/null; then
+  exec "$@"
+fi
+
+[[ "${1:-}" == "omp" ]] && shift
+
+exec omp "$@"
